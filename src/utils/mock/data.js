a7384e9cc5102fff23ce79a63aba9ef2fc470562
morteza-hoseinzadeh <morteza.hoseinzadeh553@rahaco.com>
2025-02-19 00:39:49 +0330

    Add mock data

---
+++ src/utils/mock/data.js
@@ -0,0 +1,118 @@
+export const banner = '/img/banner/cafe23.jpg';
+export const logo = '/img/logo/cafe23.png';
+export const name = 'کافه ۲۳';
+
+export const products = [
+  // نوشیدنی‌های گرم
+  {
+    name: 'اسپرسو',
+    description: 'قهوه غلیظ و پرکافئین تهیه‌شده از دانه‌های مرغوب عربیکا و روبوستا',
+    price: '۵۰,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'نوشیدنی گرم',
+  },
+  {
+    name: 'کاپوچینو',
+    description: 'ترکیب متعادل اسپرسو، شیر بخار داده شده و فوم شیر، با طعمی لطیف و خامه‌ای',
+    price: '۶۵,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'نوشیدنی گرم',
+  },
+  {
+    name: 'چای ماسالا',
+    description: 'چای سیاه هندی به همراه ترکیبی از ادویه‌های معطر مانند دارچین، زنجبیل، هل و شیر',
+    price: '۷۰,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'نوشیدنی گرم',
+  },
+  {
+    name: 'موکا',
+    description: 'اسپرسو با ترکیبی از شیر، شکلات تلخ و خامه، مناسب برای عاشقان قهوه و شکلات',
+    price: '۷۵,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'نوشیدنی گرم',
+  },
+  {
+    name: 'هات چاکلت',
+    description: 'شکلات داغ غلیظ همراه با خامه و پودر کاکائو، مناسب برای روزهای سرد',
+    price: '۶۵,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'نوشیدنی گرم',
+  },
+
+  // نوشیدنی‌های سرد
+  {
+    name: 'آیس لاته',
+    description: 'ترکیب قهوه اسپرسو، شیر سرد و یخ برای یک تجربه خنک و ملایم',
+    price: '۷۵,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'نوشیدنی سرد',
+  },
+  {
+    name: 'فراپوچینو',
+    description: 'ترکیب قهوه، یخ، شیر و سس کارامل یا شکلات، مناسب برای روزهای گرم',
+    price: '۸۵,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'نوشیدنی سرد',
+  },
+  {
+    name: 'اسموتی توت‌فرنگی',
+    description: 'ترکیب طبیعی توت‌فرنگی تازه، ماست، عسل و یخ',
+    price: '۸۰,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'نوشیدنی سرد',
+  },
+  {
+    name: 'موهیتو',
+    description: 'نوشیدنی سنتی کوبایی با ترکیب نعناع تازه، لیمو و سودا',
+    price: '۷۵,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'نوشیدنی سرد',
+  },
+
+  // دسرها
+  {
+    name: 'چیزکیک نیویورکی',
+    description: 'چیزکیک خامه‌ای با پایه بیسکویتی و سس توت‌فرنگی',
+    price: '۹۵,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'دسر',
+  },
+  {
+    name: 'براونی شکلاتی',
+    description: 'کیک براونی شکلاتی غنی با مغز گردو و رویه شکلاتی',
+    price: '۹۰,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'دسر',
+  },
+  {
+    name: 'تیرامیسو',
+    description: 'لایه‌های بیسکوییت آغشته به قهوه، خامه ماسکارپونه و پودر کاکائو',
+    price: '۱۱۰,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'دسر',
+  },
+
+  // غذاهای سبک
+  {
+    name: 'ساندویچ بوقلمون',
+    description: 'نان تازه با فیله بوقلمون، کاهو، گوجه و سس مخصوص',
+    price: '۱۳۰,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'غذا',
+  },
+  {
+    name: 'سالاد سزار',
+    description: 'ترکیب کاهو تازه، فیله مرغ گریل شده، نان تست، پنیر پارمسان و سس مخصوص سزار',
+    price: '۱۲۰,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'غذا',
+  },
+  {
+    name: 'پاستا آلفردو',
+    description: 'پاستای فتوچینی با سس آلفردو کرمی و تکه‌های مرغ گریل شده',
+    price: '۱۵۰,۰۰۰ تومان',
+    img: '/img/product/cafe23/Spresso.png',
+    category: 'غذا',
+  },
+];
