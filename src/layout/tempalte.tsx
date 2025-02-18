ca355f1b49c8acd28a1fc1fb80af4b8c43d7efa5
morteza-hoseinzadeh <morteza.hoseinzadeh553@rahaco.com>
2025-02-19 02:59:34 +0330

    Remove design

--- src/layout/tempalte.tsx
+++ src/layout/tempalte.tsx
@@ -5,7 +5,7 @@ import { TemplateProps } from '@/utils/types/type.e';
 const Template: React.FC<TemplateProps> = ({ details, data }) => {
   return (
     <>
-      <Header details={details} />
+      <Header />
     </>
   );
 };
