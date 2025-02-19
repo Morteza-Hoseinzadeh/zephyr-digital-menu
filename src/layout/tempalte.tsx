9fe5342b4ff7a1c09de0c518e4f5ddfd56d7e5e8
morteza-hoseinzadeh <morteza.hoseinzadeh553@rahaco.com>
2025-02-20 01:16:42 +0330

    Add framer motion to parent component

--- src/layout/tempalte.tsx
+++ src/layout/tempalte.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { motion } from 'framer-motion';
+
 import Header from './components/header';
 import RuleSection from './components/rules';
 
@@ -7,10 +9,10 @@ import { TemplateProps } from '@/utils/types/type.e';
 
 const Template: React.FC<TemplateProps> = ({ details, data, rules }) => {
   return (
-    <>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.3 }}>
       <Header details={details} />
       <RuleSection rules={rules} />
-    </>
+    </motion.div>
   );
 };
 
